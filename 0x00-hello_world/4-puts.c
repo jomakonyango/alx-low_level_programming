a7d9903d0edbbbb8141575bb92f1acfa6ecf1ddf
John Otieno Onyango <jomakonyango@gmail.com>
2023-08-12 02:17:07 -0700

    corrected c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 /**
  * main - Entry Point
  *
- * Returm: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 	/*This line serves as an entry point*/
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);/*INDICATES SUCCESSFUL PROGRAM EXECUTION*/
+	return (0);
 }
