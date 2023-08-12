8f6c3ef2a3e28548c3e33ae19abde5d781af64f9
John Otieno Onyango <jomakonyango@gmail.com>
2023-08-12 01:54:07 -0700

    first commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Returm: Always 0 (Success)
+ */
+int main(void)
+	/*This line serves as an entry point*/
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);/*INDICATES SUCCESSFUL PROGRAM EXECUTION*/
+}
