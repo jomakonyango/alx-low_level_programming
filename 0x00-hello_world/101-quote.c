181692126faefe5561c027c3b981de196ccf1153
John Otieno Onyango <jomakonyango@gmail.com>
2023-08-13 13:58:50 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <unstd.h>
 /**
- * main - Entry point
+ * main - C program that prints new line, to the standard error.
  *
  * Return: Always 1 (Success)
  */
 int main(void)
 {
-	write(2,"and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59");
+	fput("and that piece of art is useful\" - Dora Korp
+ar, 2015-10-19\n", stdout);
 	return (1);
 }
