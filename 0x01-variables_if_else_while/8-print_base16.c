7769b19b30bab6bc45e31a6e2af210e9e70fecdd
John Otieno Onyango <jomakonyango@gmail.com>
2023-08-14 20:11:04 -0700

    Hexadecimal print

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,18 +5,17 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void) {
+int main(void)
+{
 	int i;
 
 	/* Print numbers 0 to 9 */
-	for (i = 0; i < 10; i++) {
+	for (i = 0; i < 10; i++)
 	putchar('0' + i);
-	}
 
     /* Print lowercase letters a to f */
-	for (i = 0; i < 6; i++) {
+	for (i = 0; i < 6; i++)
 	putchar('a' + i);
-}
 
 	/* Print a new line */
 	putchar('\n');
