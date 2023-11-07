529a248db06e27c8eeeb7ef0b2165a98f502fcbf
John Otieno Onyango <jomakonyango@gmail.com>
2023-11-07 10:21:26 -0800

    commit macro

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/*
+ * ifndef OBJECT_LIKE_MACRO_H - Check if OBJECT_LIKE_MACRO_H
+ * is not defined
+ *  define OBJECT_LIKE_MACRO_H - Define OBJECT_LIKE_MACRO_H
+ */
+
+#define SIZE 1024
+
+#endif
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,2 @@
+0x0D. C - Preprocessor
+learning how to declare and work with macros in C
