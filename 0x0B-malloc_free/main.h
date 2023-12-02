cfb0fdc0683e99143658487f5619f36afd78ee55
John Otieno Oyango <jomakonyango@gmail.com>
2023-12-02 04:56:05 -0800

    Strup

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,7 +1,8 @@
 #ifndef	MAIN_H
 #define	MAIN_H
 
-char	*create_array(unsigned	int	size,	char	c);
-int	_putchar(char	c);
+char *create_array(unsigned	int	size,	char	c);
+int _putchar(char	c);
+char *_strdup(char *str);
 
 #endif	/*	MAIN_H	*/
