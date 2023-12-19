a1fcfedcc336938ef97ca92193a5547fa5c418b8
John Otieno Oyango <jomakonyango@gmail.com>
2023-12-19 12:32:20 -0800

    correction

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,20 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - prints 10 times the alphabet, in lowercase, followed by a new line
+ * print_alphabet_x10 - prints 10 times the alphabet
+ * in lowercase, followed by a new line
  */
 void print_alphabet_x10(void)
 {
 int i;
 char letter;
 
-for (i = 0; i < 10; i++)
-{
-for (letter = 'a'; letter <= 'z'; letter++)
-{
- _putchar(letter);
+	for (i = 0; i < 10; i++)
+		{
+			for (letter = 'a'; letter <= 'z'; letter++)
+				{
+					 _putchar(letter);
+				}
+			_putchar('\n');
+		}
 }
-
-_putchar('\n');
-}
-}
-
