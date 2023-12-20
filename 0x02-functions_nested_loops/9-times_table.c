051dab56bf8a0cc9febfae1df75e84853d8a45fd
John Otieno Oyango <jomakonyango@gmail.com>
2023-12-20 02:57:55 -0800

    prints the 9 times table, starting with 0

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,33 +1,38 @@
 #include "main.h"
+
 /**
- * time_table - prints 9 times the table ,starting with 0
- */
-void time_tables(void)
+* times_table - prints the 9 times table, starting with 0
+*/
+void times_table(void)
 {
-	int i,j,k;
-	for (i = 0; i < 10; i++)
-	{
-		for (j = 0; j < 10; j++)
-			k = j * 1;
-			if (j == 0)
-		{
-			_putchar(k + '0');
-		}
-				if (k < 10 && j!= 0)
-				{
-					_putchar(',);
-			_putchar('');
-			_putchar('');
-			_putchar(k + '0');
-		}
-		else if (k >= 10)
-		{
-			_putchar(',);
-                        _putchar('');
-			_putchar((k / 10) + '0');
-                       _putchar((k % 10) + '0');
-		}
-	}
-		_putchar('\n');
+int i, j, product;
+
+for (i = 0; i <= 9; i++)
+{
+for (j = 0; j <= 9; j++)
+{
+product = i * j;
+if (j != 0)
+{
+_putchar(',');
+_putchar(' ');
+if (product >= 10)
+{
+_putchar(product / 10 + '0');
+_putchar(product % 10 + '0');
+}
+else
+{
+_putchar(' ');
+_putchar(product + '0');
 }
 }
+else
+{
+_putchar(product + '0');
+}
+}
+_putchar('\n');
+}
+}
+
