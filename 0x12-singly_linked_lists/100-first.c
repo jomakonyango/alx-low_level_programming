b657ac9ac1705d26167109ae1691416a0783faab
John Otieno Oyango <jomakonyango@gmail.com>
2024-01-01 07:04:18 -0800

    yo are beat

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * premain - Prints a specific string before the main function is executed.
+ */
+
+void __attribute__ ((constructor)) premain()
+{
+printf("You're beat! and yet, you must allow,\n"
+"I bore my house upon my back!\n");
+}
